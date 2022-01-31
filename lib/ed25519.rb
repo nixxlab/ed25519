ce147289d1a62a7fef82e8b3150178f31878c9e5
Nick Nizovtsev <nizovtsevnv@gmail.com>
2022-01-31 20:00:54 +0300

    Temporary remove self_test

--- lib/ed25519.rb
+++ lib/ed25519.rb
@@ -29,29 +29,4 @@ module Ed25519
 
   KEY_SIZE = 32
   SIGNATURE_SIZE = 64
-
-  # Perform a self-test to ensure the selected provider is working
-  def self_test
-    signing_key = Ed25519::SigningKey.new("A" * 32)
-    raise SelfTestFailure, "failed to generate verify key correctly" unless signing_key.verify_key.to_bytes.unpack1("H*") == "db995fe25169d141cab9bbba92baa01f9f2e1ece7df4cb2ac05190f37fcc1f9d"
-
-    message = "crypto libraries should self-test on boot"
-    signature = signing_key.sign(message)
-    raise SelfTestFailure, "failed to generate correct signature" unless signature.unpack1("H*") == "c62c12a3a6cbfa04800d4be81468ef8aecd152a6a26a81d91257baecef13ba209531fe905a843e833c8b71cee04400fa2af3a29fef1152ece470421848758d0a"
-
-    verify_key = signing_key.verify_key
-    raise SelfTestFailure, "failed to verify a valid signature" unless verify_key.verify(signature, message)
-
-    bad_signature = "#{signature[0...63]}X"
-    ex = nil
-    begin
-      verify_key.verify(bad_signature, message)
-    rescue Ed25519::VerifyError => ex
-    end
-
-    raise SelfTestFailure, "failed to detect an invalid signature" unless ex.is_a?(Ed25519::VerifyError)
-  end # self_test
 end # Ed25519
-
-# Automatically run self-test when library loads
-Ed25519.self_test
